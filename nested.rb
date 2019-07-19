b0fe499897b12ae1a44395b4e7790a1b6eca2180
German Garcia <ggarciag.business@outlook.com>
2019-07-19 16:41:50 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,12 +15,14 @@ def hopper
           :languages => ["C"]
         }
      }
-
+          return programmer_hash[:grace_hopper]
+          return programmer_hash[:alan_kay]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+# 	programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,17 +58,23 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    
+     return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+#     :languages => ["LISP", "C"]
 # }
 
 	programmer_hash = 
  		{
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+ 		    },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -100,7 +109,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     return programmer_hash
      
      
 end
@@ -120,6 +130,8 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+ 		}
+        programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+        
+        return programmer_hash
 end
